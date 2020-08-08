206f7c70113015d8cf1780e9e2be6024ca15312c
Ananya Heroor <47873321+AnanyaHeroor@users.noreply.github.com>
2020-08-08 13:43:56 -0700

    Update MyFirstProgram.java

--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -1,3 +1,7 @@
+/**
+* This class uses a print statement
+* @author Ananya Heroor
+*/
 public class MyFirstProgram{
     public static void main (String[] args){
         System.out.println("It's Aliiiiiive!");
