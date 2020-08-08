5fac677316e3e73b5645d57ca53a1e886e912368
Ananya Heroor <47873321+AnanyaHeroor@users.noreply.github.com>
2020-08-08 12:09:39 -0700

    Add files via upload

---
+++ Art.java
@@ -0,0 +1,10 @@
+public class Art{
+    public static void main (String[] args){
+        System.out.println(",-.___,-.");
+        System.out.println("  |_ _| ");
+        System.out.println("  )O_O(  ");
+        System.out.println(" { (_) } ");
+        System.out.println("  `-^-'  ");
+        //my attempt at making a bear's face? It looks like a dog but also a bear :(
+    }
+}
---
+++ MySecondProgram.java
@@ -0,0 +1,6 @@
+public class MySecondProgram{
+    public static void main (String[] args){
+        System.out.println("If this code doesn't work then I will be sad");
+        System.out.println("If this code works then I will be happy");
+    }
+}
